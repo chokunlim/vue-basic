42ee39be5d8fb159d3577be72b21302872373aad
chokunlim <puri.li@ku.th>
2024-10-03 00:04:07 +0700

    vue components, template, props, state & calling rest api

---
+++ src/components/MyCard.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div class="card">
+        <img :src="coverimage" :alt="name" style="width:100%">
+        <div class="container">
+            <h3><b>{{name}}</b></h3> 
+            <p>{{detail}}</p> 
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  props: ['id', 'name', 'detail', 'coverimage']
+}
+</script>
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
+</style>
